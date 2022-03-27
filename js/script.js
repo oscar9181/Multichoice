efc3b5c1f505b60c2badcee735f42c937a6ef708
oscar9181 <oscarokola17@gmail.com>
2022-03-27 22:48:38 +0300

    add changes in js and css index.html

--- js/script.js
+++ js/script.js
@@ -6,34 +6,44 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function(){
+    $(".submit").click(function(event){
+        $(".container").hide()
+   
+   
+    })
+})
 
     function submitButton() {
         var score = 0;
-        const correctAnswer1 = document.questionsList.question1.value;  
-        const correctAnswer2 = document.questionsList.question2.value;
-        const correctAnswer3 = document.questionsList.question3.value;
-        const correctAnswer4 = document.questionsList.question4.value;
-        const correctAnswer5 = document.questionsList.question5.value;
-        const correctAnswer6 = document.questionsList.question6.value;
-        const correctAnswer7 = document.questionsList.question7.value;
+        const correctAnswer1 = $("input[type=radio][name=question1]:checked").val();
+        const correctAnswer2 = $("input[type=radio][name=question2]:checked").val();
+        const correctAnswer3 = $("input[type=radio][name=question3]:checked").val();
+        const correctAnswer4 = $("input[type=radio][name=question4]:checked").val();
+        const correctAnswer5 = $("input[type=radio][name=question5]:checked").val();
+        const correctAnswer6 = $("input[type=radio][name=question6]:checked").val();
+        const correctAnswer7 = $("input[type=radio][name=question7]:checked").val();
 
 
         var result = document.getElementById("text");
-        if (correctAnswer1 == "3") (score++);
-        if (correctAnswer2 == "c") (score++);
-        if (correctAnswer3 == "c") (score++);
-        if (correctAnswer4 == "a") (score++);
-        if (correctAnswer5 == "a") (score++);
-        if (correctAnswer6 == "c") (score++);
-        if (correctAnswer7 == "c") (score++);
+        if (correctAnswer1 == "java") (score++);
+        if (correctAnswer2 == "1999") (score++);
+        if (correctAnswer3 == "Mark Otto") (score++);
+        if (correctAnswer4 == "YES") (score++);
+        if (correctAnswer5 == "Yes") (score++);
+        if (correctAnswer6 == "jQuery effectfadeout()method") (score++);
+        if (correctAnswer7 == "You can use JavaScript to create interactive web elements") (score++);
 
 
-
+         
         var scorePercent = (Math.floor(score * 100 / 7));
         if (scorePercent >= 80) {
-            result.textContent = score + "/7. Congratulations! You have scored " + scorePercent + "%.  EXCELLENT!!!";
+            result.innerHTML = score + "/7. Congratulations! You have scored " + scorePercent + "%.  EXCELLENT!!!";
         } else if (scorePercent >= 50) {
-            result.textContent = score + "/7. Congratulations, You have scored " + scorePercent + "% . FAIR!";
+            result.innerHTML = score + "/7. Congratulations, You have scored " + scorePercent + "% . FAIR!";
+        }
+        else{
+            result.innerHTML = score + "Try harder";
         }
     
     };
